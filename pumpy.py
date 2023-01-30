0ad97349c59aef9522ffa0cb21fad084ff4d0876
AliceVlasov <67074621+AliceVlasov@users.noreply.github.com>
2023-01-30 15:40:28 +0000

    Create pumpy.py

---
+++ pumpy.py
@@ -0,0 +1,22 @@
+from motors import Motors
+from time import time, sleep
+
+mc = Motors()
+
+# IDs for pump and valve on the motorboard
+pump_id = 1
+valve_id = 0
+
+# speed (power)
+pump_speed = 50
+valve_speed = 50
+
+# start the motor
+mc.move_motor(pump_id, pump_speed)
+sleep(5)
+
+# stop everything
+mc.stop_motor(pump_id)
+mc.move_motor(valve_id, valve_speed)
+sleep(10)
+mc.stop_motor(valve_id)
