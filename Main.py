f49c0db05911943234e3083a35657c8d32511032
HelloYuiYui <enes35ihsan@gmail.com>
2023-02-01 16:20:21 +0000

    Added a simple button

---
+++ Main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from GUI import Window
+
+root = Tk()
+app = Window(root)
+root.wm_title("Tkinter button")
+root.geometry("320x200")
+root.resizable(False, False)
+root.mainloop()
