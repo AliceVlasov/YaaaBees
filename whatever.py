a53473968e59c0dfb0524fe0a78d611300329176
AliceVlasov <alicev.canada@gmail.com>
2023-03-10 13:48:53 +0000

    new valve stuff

---
+++ whatever.py
@@ -0,0 +1,24 @@
+from Air import Pump, Silicone_valve, Pump_valve
+from time import sleep
+
+pump_deflate = Pump(5, 80)
+pump_inflate = Pump(4, 100)
+pump_valve = Pump_valve(2, "pump_valve")
+silicone_valve = Silicone_valve(3, "silicone_valve")
+
+# inflating
+pump_valve.open_inflate()
+pump_inflate.run()
+sleep(5)
+pump_inflate.stop()
+
+#deflating
+pump_valve.open_deflate()
+pump_deflate.run()
+sleep(5)
+pump_deflate.stop()
+
+sleep(2)
+
+pump_valve.open_deflate()
+
